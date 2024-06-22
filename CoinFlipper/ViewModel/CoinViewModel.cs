c247a59763d3eae9df8698eb6ca2417d0c4f2eeb
luish <luish@LUIS>
2024-06-22 11:35:16 -0300

    Adicionar arquivos de projeto.

---
+++ CoinFlipper/ViewModel/CoinViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoinFlipper.Models;
+using System.Windows.Input;
+
+
+
+namespace CoinFlipper.ViewModel
+{
+    public partial class CoinViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        private string resultado;
+
+        [ObservableProperty]
+        private string imagem;
+
+        [ObservableProperty]
+        private string escolha;
+
+        public ICommand JogarCommand { get; }
+
+        public CoinViewModel()
+        {
+            JogarCommand = new RelayCommand(Jogar);
+        }
+
+        public void Jogar()
+        {
+            if (String.IsNullOrEmpty(Escolha)) {
+                App.Current.MainPage.DisplayAlert("Cara ou coroa", "Selecione cara ou coroa para prosseguir", "ok");
+            } else
+            {
+                Coin coin = new Coin();
+                coin.Jogar();
+
+                Resultado = Escolha == coin.LadoSorteado ? "Parabéns, você acertou" : "Você perdeu";
+
+                Imagem = coin.LadoSorteado == "cara" ? "cara.png" : "coroa.png";
+            }
+
+            
+        }
+    }
+}
